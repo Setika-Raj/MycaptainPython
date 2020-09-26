db873f20246b03fd8426d0cb0f76d0344660849f
SETIKA RAJ <65080319+Setika-Raj@users.noreply.github.com>
2020-09-26 22:28:48 +0530

    Add files via upload

---
+++ positive-numbers.py
@@ -0,0 +1,16 @@
+list1=[12,-7,5,64,-14]
+output1=[]
+for number in list1:
+    if number>0:
+        output1.append(number)
+        
+print(*output1,sep=",")
+
+list2=[12,14,-95,3]
+output2=[]
+for number in list2:
+    if number>0:
+        output2.append(number)
+        
+print(output2)
+
